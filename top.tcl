55c1d70e3496cda136fd2f05a7fed71a046fd00f
Ralph Loader <ralph1loader@gmail.com>
2020-05-30 08:46:38 +1200

    Make the board repo explicit.

--- top.tcl
+++ top.tcl
@@ -1,6 +1,11 @@
 # vivado -source top.tcl
 
+set_param board.repoPaths /home/mirror/XilinxBoardStore
+
 create_project Gogo . -part xc7a100tcsg324-2
+
+set_property BOARD_PART_REPO_PATHS /home/mirror/XilinxBoardStore [current_project]
+
 set_property board_part trenz.biz:te0710_02_100_2c:part0:1.0 [current_project]
 update_ip_catalog
 
@@ -18,25 +23,25 @@ update_compile_order -fileset sources_1
 launch_runs synth_1 -jobs 8
 
 # While we waiting for synthesis, open up the hw manager.
-open_hw_manager
-connect_hw_server -url localhost:3121 -allow_non_jtag
-current_hw_target [get_hw_targets */xilinx_tcf/Digilent/251633000000A]
-open_hw_target
-set_property PARAM.FREQUENCY 5000000 [get_hw_targets]
-set a100 [lindex [get_hw_devices xc7a100t_0] 0]
-current_hw_device $a100
-refresh_hw_device $a100
-create_hw_cfgmem -hw_device $a100 [lindex [get_cfgmem_parts {s25fl256sxxxxxx0-spi-x1_x2_x4}] 0]
+#open_hw_manager
+#connect_hw_server -url localhost:3121 -allow_non_jtag
+#current_hw_target [get_hw_targets */xilinx_tcf/Digilent/251633000000A]
+#open_hw_target
+#set_property PARAM.FREQUENCY 5000000 [get_hw_targets]
+#set a100 [lindex [get_hw_devices xc7a100t_0] 0]
+#current_hw_device $a100
+#refresh_hw_device $a100
+#create_hw_cfgmem -hw_device $a100 [lindex [get_cfgmem_parts {s25fl256sxxxxxx0-spi-x1_x2_x4}] 0]
 
-set cfgmem [get_property PROGRAM.HW_CFGMEM $a100]
-set_property PROGRAM.ADDRESS_RANGE  {entire_device} $cfgmem
-set_property PROGRAM.FILES [list "../peta/Gogo/images/linux/boot.mcs"] $cfgmem
-set_property PROGRAM.UNUSED_PIN_TERMINATION {pull-none} $cfgmem
-set_property PROGRAM.BLANK_CHECK  0 $cfgmem
-set_property PROGRAM.ERASE  1 $cfgmem
-set_property PROGRAM.CFG_PROGRAM  1 $cfgmem
-set_property PROGRAM.VERIFY  1 $cfgmem
-set_property PROGRAM.CHECKSUM  0 $cfgmem
+#set cfgmem [get_property PROGRAM.HW_CFGMEM $a100]
+#set_property PROGRAM.ADDRESS_RANGE entire_device $cfgmem
+#set_property PROGRAM.FILES [list "../peta/Gogo/images/linux/boot.mcs"] $cfgmem
+#set_property PROGRAM.UNUSED_PIN_TERMINATION pull-none $cfgmem
+#set_property PROGRAM.BLANK_CHECK  0 $cfgmem
+#set_property PROGRAM.ERASE  1 $cfgmem
+#set_property PROGRAM.CFG_PROGRAM  1 $cfgmem
+#set_property PROGRAM.VERIFY  1 $cfgmem
+#set_property PROGRAM.CHECKSUM  0 $cfgmem
 
 
 wait_on_run synth_1
