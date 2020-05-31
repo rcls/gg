353be817ea02fc4d9e32cb733f3ca22dcc9cf034
Ralph Loader <ralph1loader@gmail.com>
2020-05-31 13:19:42 +1200

    Reopen project correctly

--- impl.tcl
+++ impl.tcl
@@ -1,5 +1,7 @@
 # vivado -source top.tcl
 
+open_project Gogo
+
 launch_runs impl_1 -to_step write_bitstream -jobs 4
 wait_on_run impl_1
 write_hw_platform -fixed -force -include_bit -file Go_wrapper.xsa
--- synth.tcl
+++ synth.tcl
@@ -1,4 +1,6 @@
 # vivado -source top.tcl
 
+open_project Gogo
+
 launch_runs synth_1 -jobs 4
 wait_on_run synth_1
